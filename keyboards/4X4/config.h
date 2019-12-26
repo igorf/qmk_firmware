78b8f9190e01d7dacb4c72ce90306800da453ff6
igor <pointer@nm.ru>
2019-12-26 20:07:12 +0400

    [X] increase debounce [X] add mute [X] add brightness control [X] add reset key

--- keyboards/4X4/config.h
+++ keyboards/4X4/config.h
@@ -31,7 +31,8 @@
 #endif
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
+/* Default debounce is 5 */
+#define DEBOUNCE 10
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
